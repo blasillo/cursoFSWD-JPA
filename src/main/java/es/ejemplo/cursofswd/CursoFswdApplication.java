fd99ae5db796a76e0e57fa67ab56655b16e6af07
unknown <btorregrosa@turia.infor.uva.es>
2022-09-27 17:55:11 +0200

    Inicio

---
+++ src/main/java/es/ejemplo/cursofswd/CursoFswdApplication.java
@@ -0,0 +1,13 @@
+package es.ejemplo.cursofswd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoFswdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoFswdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/ejemplo/cursofswd/CursoFswdApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ejemplo.cursofswd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoFswdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
